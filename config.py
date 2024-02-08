ced3ed6101bb25a5f04696c43a86377e6f8dd7cd
Estebanx6 <85512934+Estebanx6@users.noreply.github.com>
2024-02-07 18:36:45 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+## General settings
+bot_token = "bot.token@discord" # set the token of the bot
+discord_status = "Moderating" #Set the perfil status of the bot
+prefix = "/" # Set the prefix for the  command´s bot
+
+
+# Channels settings
+welcome_channel_id= 12345678987654321 # Set the channel´s id of the hub to joins and leaves
+strikes_channel_id = 12345678987654321 # Set the channel´s id of the strikes
+ticket_logs_channel_id = 12345678987654321 #  Set the channel´s id of the system logs
+
+welcome_message = True #Define if you want have welcome messsage
+
+# Roles settings
+roles = ["Owner", "Admin", "Mod", "@everyone"] # Set the roles to auto change the nick with the prefix of roll, keep the @everyone in Last Position
+All_commands_role = "Owner" #Set the all commands role
+ticket_viewer_role = "Admin" #Set the viewer tickets role
+
+
+## Strikes settings 
+direct_message_strike = False #Define if the staff will receive a direct message of the bot
+strike_embed = False #Define if you want strike message with embed
+strike_embed_color = 0xFF0000 #Define the color of the embed
+
+
+
+
